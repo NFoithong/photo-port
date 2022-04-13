a31a8660ab80a47daeb38128bddf55612f3b1d8f
Josh <jzieve@Joshs-iMac.local>
2022-04-13 16:33:00 -0400

    add useEffect on Nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,32 +1,12 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Nav(props) {
 
-    // const [categories] = useState([
-    //     {
-    //         name: 'commercial',
-    //         description: 'Photo of grocery stores, food trucks, and other commercial projects'
-    //     },
-    //     {
-    //         name: 'portrait',
-    //         description: 'Portraits of people in my life'
-    //     },
-    //     {
-    //         name: 'food',
-    //         description: 'Delicious delicacies'
-    //     },
-    //     {
-    //         name: 'landscape',
-    //         description: 'Fields, farmhouses, waterfalls, and the beauty of nature'
-    //     },
-    // ]);
+    useEffect(()=>{
+        document.title = capitalizeFirstLetter(currentCategory.name);
+    }, [currentCategory]);
 
-    // const [currentCategory, setCurrentCategory] = useState(categories[0])
-
-    // function categorySelected(name) {
-    //     console.log(`${name} clicked`);
-    // }
     const {
         categories = [],
         setCurrentCategory,
@@ -37,7 +17,12 @@ function Nav(props) {
         <header className="flex-row px-1">
             <h2>
                 <a data-testid='link' href="/">
-                    <span role="img" aria-label="camera">{" "}📸</span>{" "}Oh Snap!
+                    <span role="img" aria-label="camera">
+                        {" "}
+                        📸
+                        </span>
+                        {" "}
+                        Oh Snap!
                 </a>
             </h2>
             <nav>
@@ -53,9 +38,9 @@ function Nav(props) {
                     {categories.map((category)=>(
                         <li 
                         className={`mx-1 ${
-                            currentCategory.name === category.name && 'nacActive'
+                            currentCategory.name === category.name && 'navActive'
                         }`} 
-                        key={category.name}
+                        // key={category.name}
                         >
                             <span 
                             onClick={()=>{
